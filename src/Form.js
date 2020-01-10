04c8bd095a06e81f05eeaf5d81f502f792f6d419
Elizabeth Le <elizabethlemk@yahoo.com>
2020-01-10 16:24:28 -0500

    form

---
+++ src/Form.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const Form = props => {
+  const [username, setUsername] = useState("");
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    fetch(`https://api.github.com/users/${username}/repos`)
+      .then(resp => resp.json())
+      .then(data => {
+        console.log(data);
+        // props.onSubmit(data);
+        setUsername("");
+      });
+  };
+  return (
+    <form className="username-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={username}
+        onChange={event => setUsername(event.target.value)}
+        placeholder="GitHub username"
+        required
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
