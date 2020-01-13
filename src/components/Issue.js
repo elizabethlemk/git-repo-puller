7d9d3058060aaf12452457bb934b486aee1569c2
Elizabeth Le <elizabethlemk@yahoo.com>
2020-01-12 20:38:20 -0500

    time formatting

--- src/components/Issue.js
+++ src/components/Issue.js
@@ -1,14 +1,10 @@
 import React from "react";
+import Moment from "react-moment";
 
 const Issue = props => {
   console.log(props);
   let issue = props.issue;
 
-  // let created_date = new Date(issue.created_at);
-  // created_date = created_date.toLocaleFormat("%m/%d/%Y");
-  // let updated_date = new Date(issue.updated_at);
-  // updated_date = `${Date.now() - updated_date}`;
-  console.log(new Date(issue.created_at));
   return (
     <div
       className="item flex"
@@ -25,8 +21,16 @@ const Issue = props => {
       <div>
         <ul>
           <li>{issue.title}</li>
-          <li>Created at: {issue.created_at} </li>
-          {issue.updated_at ? <li>Updated at: {issue.updated_at} </li> : null}
+          <span>
+            Created at:{" "}
+            {<Moment format="MM/DD/YYYY" date={new Date(issue.created_at)} />}
+          </span>
+          {"   "}
+          {issue.updated_at ? (
+            <span>
+              Updated at: {<Moment fromNow date={new Date(issue.updated_at)} />}{" "}
+            </span>
+          ) : null}
         </ul>
       </div>
     </div>
