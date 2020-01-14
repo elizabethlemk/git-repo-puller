7012619ad6800bc4524da9d3b593eb18f00b10f8
Elizabeth Le <elizabethlemk@yahoo.com>
2020-01-13 21:06:26 -0500

    tets

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { configure, shallow } from "enzyme";
 import App from "./App";
+import Form from "./components/Form";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
 
-test("It should render the submit button", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Submit/i);
-  expect(linkElement).toBeInTheDocument();
+test("The form button text to equal 'Submit'", () => {
+  const formButton = shallow(<Form />);
+  expect(formButton.text()).toEqual("Submit");
 });
